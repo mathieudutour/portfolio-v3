9ae263dc33a018f2dc07e7716e7c19376f24bee8
Mathieu Dutour <mathieu@dutour.me>
2014-10-31 10:28:21 +0000

    obviously needed to deploy to heroku

---
+++ app/config/index.js
@@ -0,0 +1,25 @@
+/*jshint node: true*/
+exports.config = {
+  siteURL: "http://localhost:3000",
+  facebook: {
+    clientID: '346040448909652',
+    clientSecret: 'bd815c44db5cff3437d71af27c8bc69f'
+  },
+  strava: {
+    clientID: '3439',
+    clientSecret: '8b60bd2e5797d8e327fc8ed1055e9c990c64b46b'
+  },
+  twitter: {
+    consumerKey: 'lgiadg6aHE9hp5ga75TrKhil2',
+    consumerSecret: 'S6Wtwc6dONJ24VaM518BqLZmLBIAaTIqMVemKgjqN8iiPG09J3'
+  },
+  github: {
+    clientID: 'bde1aa08409be679edd9',
+    clientSecret: 'af7b325388f315b48999f2560f39f312d43fb474'
+  },
+  linkedin: {
+    consumerKey: '75jl1oy2kd1jzp',
+    consumerSecret: 'iybnchpVIC10SfU9'
+  },
+  rescuetime: 'B63P6JWu25f2wNujn_9KGrrmzukFTii1656Aelms'
+};
