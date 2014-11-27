0766e6f946072ed0033696d7bbbcf6ba986f7caf
Mathieu Dutour <mathieu@dutour.me>
2014-11-27 12:25:14 +0000

    not working...

--- app/controllers/strategies/rescuetime.js
+++ app/controllers/strategies/rescuetime.js
@@ -5,8 +5,9 @@ var Cron = require('cron').CronJob;
 
 var apiKey = function (req, res) {
   var api_key = req.body.api_key;
+  console.log(api_key);
   var today = new Date().getFullYear() + '-' + (new Date().getMonth() + 1) + '-' + new Date().getDate();
-  request("https://www.rescuetime.com/anapi/data?format=json&key="+api_key+"&restrict_begin=2000-01-01&restrict_end="+today, function (error, response, body) {
+  request("https://www.rescuetime.com/anapi/data?format=json&key="+api_key+"&restrict_begin=2000-01-01&restrict_end="+today, function (error, response) {
     if (!error && response.statusCode == 200) {
       db.users.update({_id: req.user._id}, {$set: {'providers.rescuetime.api_key': api_key}}, function() {
         return res.redirect('/admin');
